18d26a7d98bec921d044f9a6d50e91e89ec2af42
sdavitt <sdavt2@gmail.com>
2021-11-30 13:57:08 -0600

    hw review

---
+++ test_module.py
@@ -0,0 +1,14 @@
+def squareFootage(l, w):
+    """
+    a function to calculate the square footage of a house
+    needs the house's length and width
+    """
+    return l*w
+
+def circumference(r):
+    """
+    a function to calculate the circumference of a circle based on the circle's RADIUS
+    needs radius as input
+    """
+    from math import pi
+    return  2*pi*r
